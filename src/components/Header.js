c3e7a0c8f9b4856de200698b08a7b723ee46daf1
jheave <jimenezjheave123@gmail.com>
2022-08-20 00:04:23 +0800

    add memegenerator

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-
     render() {
         return (
             <header>this is a header</header>
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class MemeGenerator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>meme generator</div>
+        )
+
+
+    }
+}
