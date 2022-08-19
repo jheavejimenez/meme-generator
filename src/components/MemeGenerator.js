d5f32ec85471ead5331d0b09bcb57f59ecd6e0bf
jheave <jimenezjheave123@gmail.com>
2022-08-20 01:37:05 +0800

    fix loading logic

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -9,51 +9,58 @@ class MemeGenerator extends React.Component {
             bottomText: "",
             randomImage: RANDOM_IMG_URL,
             allMemeImages: [],
-            isLoading: false
+            isLoading: true
         }
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     componentDidMount() {
-        this.setState({isLoading: true});
+        this.setState({isLoading: false});
         fetch(BASE_URL).then(r => r.json()).then(response => {
             this.setState({allMemeImages: response.data.memes})
         });
     }
 
-    handleInputChange (e) {
-       this.setState({
-           [e.target.name]: e.target.value
-       })
+    handleInputChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
 
     }
 
     render() {
+        const isLoading = this.state.isLoading;
+
         return (
             <div>
-                <form className={"meme-form"}>
-                    <input
-                        type={"text"}
-                        name={"topText"}
-                        placeholder={"Top Text"}
-                        value={ this.state.topText}
-                        onChange={this.handleInputChange}
-                    />
-                    <input
-                        type={"text"}
-                        name={"bottomText"}
-                        placeholder={"Bottom Text"}
-                        value={ this.state.bottomText}
-                        onChange={this.handleInputChange}
-                    />
-                    <button>Generate</button>
-                </form>
+                {isLoading
+                    ? <div>loading...</div>
+                    : <div>
+                        <form className={"meme-form"}>
+                            <input
+                                type={"text"}
+                                name={"topText"}
+                                placeholder={"Top Text"}
+                                value={this.state.topText}
+                                onChange={this.handleInputChange}
+                            />
+                            <input
+                                type={"text"}
+                                name={"bottomText"}
+                                placeholder={"Bottom Text"}
+                                value={this.state.bottomText}
+                                onChange={this.handleInputChange}
+                            />
+                            <button>Generate</button>
+                        </form>
 
-                <div className="meme">
-                    <img src={this.state.randomImg} alt="" />
-                    <h2 className="top">{this.state.topText}</h2>
-                    <h2 className="bottom">{this.state.bottomText}</h2>
-                </div>
+                        <div className="meme">
+                            <img src={this.state.randomImage} alt=""/>
+                            <h2 className="top">{this.state.topText}</h2>
+                            <h2 className="bottom">{this.state.bottomText}</h2>
+                        </div>
+                    </div>
+                }
             </div>
         );
     }
