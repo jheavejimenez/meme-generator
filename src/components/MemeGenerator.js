f61100ce4c6e8cbcca62edd55f558f9b7a01013d
jheave <jimenezjheave123@gmail.com>
2022-08-20 01:17:07 +0800

    added input

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BASE_URL} from "../constants/ApiUrl";
+import {BASE_URL, RANDOM_IMG_URL} from "../constants/ApiUrl";
 
 class MemeGenerator extends React.Component {
     constructor(props) {
@@ -7,20 +7,39 @@ class MemeGenerator extends React.Component {
         this.state = {
             topText: "",
             bottomText: "",
-            randomImage: "",
+            randomImage: RANDOM_IMG_URL,
+            allMemeImages: [],
             isLoading: false
         }
     }
     componentDidMount() {
         this.setState({isLoading: true});
         fetch(BASE_URL).then(r => r.json()).then(response => {
-            console.log(response.data.memes);
+            this.setState({allMemeImages: response.data.memes})
         });
     }
 
     render() {
         return (
-            <div>meme generator</div>
+            <div>
+                <form className={"meme-form"}>
+                    <input
+                        type={"text"}
+                        name={"topText"}
+                        placeholder={"Top Text"}
+                        value={ this.state.topText}
+
+                    />
+                    <input
+                        type={"text"}
+                        name={"bottomText"}
+                        placeholder={"Bottom Text"}
+                        value={ this.state.bottomText}
+
+                    />
+                    <button>Generate</button>
+                </form>
+            </div>
         );
     }
 }
